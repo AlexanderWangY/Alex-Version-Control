ca07af2e19ca486fb6823915ba34292ef4dbea57
TheGreekAthlete <149018506+TheGreekAthlete@users.noreply.github.com>
2023-10-31 14:15:20 -0400

    Create decode.py

---
+++ decode.py
@@ -0,0 +1,4 @@
+# Written by Lucas Gavrielides
+# Takes a password and decodes it using a reverse operation from encode
+def decode(password):
+    return ''.join([str((int(item) - 3) % 10) for item in password])
